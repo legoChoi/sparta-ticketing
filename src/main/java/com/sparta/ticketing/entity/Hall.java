438d022eb550a588ab402f363bc1e414664e828b
afkorigin <gnstldgk@naver.com>
2025-01-31 18:59:32 +0900

    add: Hall entity add

--- src/main/java/com/sparta/ticketing/entity/Hall.java
+++ src/main/java/com/sparta/ticketing/entity/Hall.java
@@ -1,4 +1,27 @@
 package com.sparta.ticketing.entity;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
 public class Hall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hall_name")
+    private String name;
+    @Column(name = "location")
+    private String location;
+
+    public Hall(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public static Hall from(String name, String location) {
+        return new Hall(name, location);
+    }
 }
