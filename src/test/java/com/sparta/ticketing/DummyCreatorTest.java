0313b43c4a69372ea1a4c6603d1d87a11d4c7ac0
legoChoi <chlx159@gmail.com>
2025-02-03 14:38:04 +0900

    feat: reservation concurrency issue test

---
+++ src/test/java/com/sparta/ticketing/DummyCreatorTest.java
@@ -0,0 +1,40 @@
+package com.sparta.ticketing;
+
+import com.sparta.ticketing.entity.Concert;
+import com.sparta.ticketing.entity.Hall;
+import com.sparta.ticketing.entity.Seats;
+import com.sparta.ticketing.entity.Session;
+import com.sparta.ticketing.repository.concert.ConcertRepository;
+import com.sparta.ticketing.repository.hall.HallRepository;
+import com.sparta.ticketing.repository.reservation.ReservationRepository;
+import com.sparta.ticketing.repository.seats.SeatsRepository;
+import com.sparta.ticketing.repository.session.SessionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class DummyCreatorTest {
+
+    @Autowired ConcertRepository concertRepository;
+    @Autowired HallRepository hallRepository;
+    @Autowired SeatsRepository seatsRepository;
+    @Autowired SessionRepository sessionRepository;
+
+    @Test
+    void createDummy() {
+        Concert concert = new Concert("concert");
+        concertRepository.save(concert);
+
+        Hall hall = new Hall("hall", "seoul");
+        hallRepository.save(hall);
+
+        for (int i = 1; i <= 10; i++) {
+            seatsRepository.save(new Seats(hall, i));
+        }
+
+        sessionRepository.save(new Session(hall, concert, LocalDateTime.now(), LocalDateTime.now()));
+    }
+}
--- src/test/java/com/sparta/ticketing/service/reservation/ReservationServiceTest.java
+++ src/test/java/com/sparta/ticketing/service/reservation/ReservationServiceTest.java
@@ -1,9 +1,59 @@
 package com.sparta.ticketing.service.reservation;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(MockitoExtension.class)
-public class ReservationServiceTest {
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class ReservationServiceTest {
+
+    @Autowired ReservationService reservationService;
+
+    @Test
+    void success1() {
+        reservationService.addReservation(1L, 1L, "tt");
+
+
+    }
+
+    @Test
+    void test() throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(1000);
+        CountDownLatch latch = new CountDownLatch(1000);
+
+        AtomicInteger successCnt = new AtomicInteger();
+        AtomicInteger failCnt = new AtomicInteger();
+
+        for (int i = 0; i < 1000; i++) {
+            executor.submit(() -> {
+                try {
+                    reservationService.addReservation(1L, 1L, "user");
+                    successCnt.incrementAndGet();
+                } catch (Exception e) {
+                    failCnt.incrementAndGet();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        executor.shutdown();
+
+        System.out.println("successCnt = " + successCnt);
+        System.out.println("failCnt = " + failCnt);
+
+        Assertions.assertThat(successCnt.get()).isEqualTo(1);
+        Assertions.assertThat(failCnt.get()).isEqualTo(999);
+    }
 }
