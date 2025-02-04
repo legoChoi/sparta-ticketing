9814e4a45c026855afc00502e236b964751fa4dd
mikejigglypuff <k24555569@gmail.com>
2025-02-04 16:59:06 +0900

    add: add test for multiple seat lock

--- src/test/java/com/sparta/ticketing/service/reservation/ReservationServiceTest.java
+++ src/test/java/com/sparta/ticketing/service/reservation/ReservationServiceTest.java
@@ -57,4 +57,39 @@ class ReservationServiceTest {
         Assertions.assertThat(successCnt.get()).isEqualTo(1);
         Assertions.assertThat(failCnt.get()).isEqualTo(threadCount - 1);
     }
+
+    // 동시에 여러 좌석에 대한 예약을 요청할 때 각 좌석에 대해서만 lock이 잘 걸리는지 테스트
+    @Test
+    void multipleSeatReservationTest() throws InterruptedException {
+        int threadCount = 10;
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        AtomicInteger successCnt = new AtomicInteger();
+        AtomicInteger failCnt = new AtomicInteger();
+
+        for (int i = 0; i < threadCount; i++) {
+            int iVal = i;
+            executor.submit(() -> {
+                try {
+                    reservationService.addReservation(
+                        iVal % 2L + 1L, iVal % 2L + 1L, Thread.currentThread().getName());
+                    successCnt.incrementAndGet();
+                } catch (Exception e) {
+                    failCnt.incrementAndGet();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        executor.shutdown();
+
+        System.out.println("successCnt = " + successCnt);
+        System.out.println("failCnt = " + failCnt);
+
+        Assertions.assertThat(successCnt.get()).isEqualTo(2);
+        Assertions.assertThat(failCnt.get()).isEqualTo(threadCount - 2);
+    }
 }
