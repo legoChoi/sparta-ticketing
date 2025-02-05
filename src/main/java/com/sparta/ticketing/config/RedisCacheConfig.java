6dfba9e595f6f06913c2eb619be701c630293778
mikejigglypuff <50828241+mikejigglypuff@users.noreply.github.com>
2025-02-05 22:46:45 +0900

    update: add key pattern eviction

--- src/main/java/com/sparta/ticketing/config/RedisCacheConfig.java
+++ src/main/java/com/sparta/ticketing/config/RedisCacheConfig.java
@@ -1,6 +1,5 @@
 //package com.sparta.ticketing.config;
 //
-//import org.springframework.cache.annotation.EnableCaching;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -9,9 +8,7 @@
 //
 //import java.time.Duration;
 //
-//
 //@Configuration
-//@EnableCaching
 //public class RedisCacheConfig {
 //
 //    @Bean
