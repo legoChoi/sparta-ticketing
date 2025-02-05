fc612aab17c4cb350a10c7b4b7e01eca02057340
rlagywnd4 <rlagywnd4@gmail.com>
2025-02-05 15:32:52 +0900

    add: JsonCreator(session,concertRequest)

--- src/main/java/com/sparta/ticketing/entity/Session.java
+++ src/main/java/com/sparta/ticketing/entity/Session.java
@@ -3,7 +3,6 @@ package com.sparta.ticketing.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
