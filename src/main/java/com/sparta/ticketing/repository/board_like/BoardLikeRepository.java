14e068ace5297ac9fa9d46a905c8a82f09655c65
afkorigin <gnstldgk@naver.com>
2025-02-06 09:36:41 +0900

    update: BoardLikeRepository

---
+++ src/main/java/com/sparta/ticketing/repository/board_like/BoardLikeRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.ticketing.repository.board_like;
+
+import com.sparta.ticketing.entity.BoardLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardLikeRepository extends JpaRepository<BoardLike,Long> {
+}
