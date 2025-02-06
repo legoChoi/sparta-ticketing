6e1f2638c4c09116a67cdca21eb0b1fd89443461
legoChoi <chlx159@gmail.com>
2025-02-06 09:36:29 +0900

    fix: 테스트 코드 주석 처리

--- src/test/java/com/sparta/ticketing/DummyCreatorTest.java
+++ src/test/java/com/sparta/ticketing/DummyCreatorTest.java
@@ -1,52 +1,52 @@
-package com.sparta.ticketing;
-
-import com.sparta.ticketing.entity.Concert;
-import com.sparta.ticketing.entity.Hall;
-import com.sparta.ticketing.entity.Seat;
-import com.sparta.ticketing.entity.Session;
-import com.sparta.ticketing.repository.concert.ConcertRepository;
-import com.sparta.ticketing.repository.hall.HallRepository;
-import com.sparta.ticketing.repository.seat.SeatRepository;
-import com.sparta.ticketing.repository.session.SessionRepository;
-import com.sparta.ticketing.service.concert.ConcertConnectorInterface;
-import com.sparta.ticketing.service.concert.ConcertService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-
-@SpringBootTest
-@Transactional
-public class DummyCreatorTest {
-    @Autowired ConcertRepository concertRepository;
-    @Autowired ConcertService concertService;
-    @Autowired HallRepository hallRepository;
-    @Autowired SeatRepository seatsRepository;
-    @Autowired SessionRepository sessionRepository;
-
-    @Test
-    @Commit
-    void createDummy() {
-        Concert concert = new Concert("concert");
-        concertRepository.save(concert);
-
-        Hall hall = new Hall("hall", "seoul");
-        hallRepository.save(hall);
-
-        Session session = new Session(hall, concert, LocalDateTime.now(), LocalDateTime.now(),10);
-        sessionRepository.save(session);
-
-        for (int i = 1; i <= 10; i++) {
-            seatsRepository.save(new Seat(session, i));
-        }
-    }
-
-    @Test
-    @Commit
-    void createBulkConcerts() {
-        concertService.bulkInsert(1000);
-    }
-}
+//package com.sparta.ticketing;
+//
+//import com.sparta.ticketing.entity.Concert;
+//import com.sparta.ticketing.entity.Hall;
+//import com.sparta.ticketing.entity.Seat;
+//import com.sparta.ticketing.entity.Session;
+//import com.sparta.ticketing.repository.concert.ConcertRepository;
+//import com.sparta.ticketing.repository.hall.HallRepository;
+//import com.sparta.ticketing.repository.seat.SeatRepository;
+//import com.sparta.ticketing.repository.session.SessionRepository;
+//import com.sparta.ticketing.service.concert.ConcertConnectorInterface;
+//import com.sparta.ticketing.service.concert.ConcertService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Commit;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.time.LocalDateTime;
+//
+//@SpringBootTest
+//@Transactional
+//public class DummyCreatorTest {
+//    @Autowired ConcertRepository concertRepository;
+//    @Autowired ConcertService concertService;
+//    @Autowired HallRepository hallRepository;
+//    @Autowired SeatRepository seatsRepository;
+//    @Autowired SessionRepository sessionRepository;
+//
+//    @Test
+//    @Commit
+//    void createDummy() {
+//        Concert concert = new Concert("concert");
+//        concertRepository.save(concert);
+//
+//        Hall hall = new Hall("hall", "seoul");
+//        hallRepository.save(hall);
+//
+//        Session session = new Session(hall, concert, LocalDateTime.now(), LocalDateTime.now(),10);
+//        sessionRepository.save(session);
+//
+//        for (int i = 1; i <= 10; i++) {
+//            seatsRepository.save(new Seat(session, i));
+//        }
+//    }
+//
+//    @Test
+//    @Commit
+//    void createBulkConcerts() {
+//        concertService.bulkInsert(1000);
+//    }
+//}
--- src/test/java/com/sparta/ticketing/service/reservation/ReservationServiceTest.java
+++ src/test/java/com/sparta/ticketing/service/reservation/ReservationServiceTest.java
@@ -1,95 +1,95 @@
-package com.sparta.ticketing.service.reservation;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@Transactional
-class ReservationServiceTest {
-
-    @Autowired ReservationService reservationService;
-
-    @Test
-    void success1() {
-        reservationService.addReservation(1L, 1L, "tt");
-
-
-    }
-
-    @Test
-    void test() throws InterruptedException {
-        int threadCount = 10;
-        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
-        CountDownLatch latch = new CountDownLatch(threadCount);
-
-        AtomicInteger successCnt = new AtomicInteger();
-        AtomicInteger failCnt = new AtomicInteger();
-
-        for (int i = 0; i < threadCount; i++) {
-            executor.submit(() -> {
-                try {
-                    reservationService.addReservation(1L, 1L, Thread.currentThread().getName());
-                    successCnt.incrementAndGet();
-                } catch (Exception e) {
-                    failCnt.incrementAndGet();
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
-
-        latch.await();
-        executor.shutdown();
-
-        System.out.println("successCnt = " + successCnt);
-        System.out.println("failCnt = " + failCnt);
-
-        Assertions.assertThat(successCnt.get()).isEqualTo(1);
-        Assertions.assertThat(failCnt.get()).isEqualTo(threadCount - 1);
-    }
-
-    // 동시에 여러 좌석에 대한 예약을 요청할 때 각 좌석에 대해서만 lock이 잘 걸리는지 테스트
-    @Test
-    void multipleSeatReservationTest() throws InterruptedException {
-        int threadCount = 10;
-        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
-        CountDownLatch latch = new CountDownLatch(threadCount);
-
-        AtomicInteger successCnt = new AtomicInteger();
-        AtomicInteger failCnt = new AtomicInteger();
-
-        for (int i = 0; i < threadCount; i++) {
-            int iVal = i;
-            executor.submit(() -> {
-                try {
-                    reservationService.addReservation(
-                        iVal % 2L + 1L, iVal % 2L + 1L, Thread.currentThread().getName());
-                    successCnt.incrementAndGet();
-                } catch (Exception e) {
-                    failCnt.incrementAndGet();
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
-
-        latch.await();
-        executor.shutdown();
-
-        System.out.println("successCnt = " + successCnt);
-        System.out.println("failCnt = " + failCnt);
-
-        Assertions.assertThat(successCnt.get()).isEqualTo(2);
-        Assertions.assertThat(failCnt.get()).isEqualTo(threadCount - 2);
-    }
-}
+//package com.sparta.ticketing.service.reservation;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.concurrent.CountDownLatch;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//@Transactional
+//class ReservationServiceTest {
+//
+//    @Autowired ReservationService reservationService;
+//
+//    @Test
+//    void success1() {
+//        reservationService.addReservation(1L, 1L, "tt");
+//
+//
+//    }
+//
+//    @Test
+//    void test() throws InterruptedException {
+//        int threadCount = 10;
+//        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+//        CountDownLatch latch = new CountDownLatch(threadCount);
+//
+//        AtomicInteger successCnt = new AtomicInteger();
+//        AtomicInteger failCnt = new AtomicInteger();
+//
+//        for (int i = 0; i < threadCount; i++) {
+//            executor.submit(() -> {
+//                try {
+//                    reservationService.addReservation(1L, 1L, Thread.currentThread().getName());
+//                    successCnt.incrementAndGet();
+//                } catch (Exception e) {
+//                    failCnt.incrementAndGet();
+//                } finally {
+//                    latch.countDown();
+//                }
+//            });
+//        }
+//
+//        latch.await();
+//        executor.shutdown();
+//
+//        System.out.println("successCnt = " + successCnt);
+//        System.out.println("failCnt = " + failCnt);
+//
+//        Assertions.assertThat(successCnt.get()).isEqualTo(1);
+//        Assertions.assertThat(failCnt.get()).isEqualTo(threadCount - 1);
+//    }
+//
+//    // 동시에 여러 좌석에 대한 예약을 요청할 때 각 좌석에 대해서만 lock이 잘 걸리는지 테스트
+//    @Test
+//    void multipleSeatReservationTest() throws InterruptedException {
+//        int threadCount = 10;
+//        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+//        CountDownLatch latch = new CountDownLatch(threadCount);
+//
+//        AtomicInteger successCnt = new AtomicInteger();
+//        AtomicInteger failCnt = new AtomicInteger();
+//
+//        for (int i = 0; i < threadCount; i++) {
+//            int iVal = i;
+//            executor.submit(() -> {
+//                try {
+//                    reservationService.addReservation(
+//                        iVal % 2L + 1L, iVal % 2L + 1L, Thread.currentThread().getName());
+//                    successCnt.incrementAndGet();
+//                } catch (Exception e) {
+//                    failCnt.incrementAndGet();
+//                } finally {
+//                    latch.countDown();
+//                }
+//            });
+//        }
+//
+//        latch.await();
+//        executor.shutdown();
+//
+//        System.out.println("successCnt = " + successCnt);
+//        System.out.println("failCnt = " + failCnt);
+//
+//        Assertions.assertThat(successCnt.get()).isEqualTo(2);
+//        Assertions.assertThat(failCnt.get()).isEqualTo(threadCount - 2);
+//    }
+//}
